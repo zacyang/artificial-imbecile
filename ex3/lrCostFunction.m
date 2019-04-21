0ea169bca5cc8ffda0e32823be9d9d928057f8d9
yangyang <yangmeansyoung@gmail.com>
2019-04-21 10:13:25 +1000

    ex3 Regularized Logistic Regression

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,11 +36,13 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X * theta);
+first_theta_to_zeror = [ [ 0 ]; theta([2:length(theta)])];
+tmp = -y .* log(h)- (1 - y) .* log(1- h);
 
+J = sum(tmp)/m + lambda * sum(first_theta_to_zeror.^2)/(2 * m);
 
-
-
-
+grad = (sum((h - y).* X )/m )+ lambda * first_theta_to_zeror'/m;
 
 
 
