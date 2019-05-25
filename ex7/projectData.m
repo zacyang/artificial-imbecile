9eb14eac546720aa3ca584c877df4f3266f40d34
yangyang <yangmeansyoung@gmail.com>
2019-05-25 10:03:50 +1000

    ex7 dimention reducation

--- ex7/projectData.m
+++ ex7/projectData.m
@@ -18,7 +18,11 @@ Z = zeros(size(X, 1), K);
 %                    projection_k = x' * U(:, k);
 %
 
-
+for i = 1: size(X, 1)
+    x = X(i, :)';
+    projection_k = x' * U(:, 1:K);
+    Z(i,:) = projection_k
+end    
 
 
 % =============================================================
